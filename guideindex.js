8558f43faa224c8fa40a1603553e8ef35457191c
L0mRR <104187091+L0mRR@users.noreply.github.com>
2022-04-24 14:13:03 +1000

    Update guideindex.js

--- guideindex.js
+++ guideindex.js
@@ -9,10 +9,58 @@ fetch('../resources/guides.json')
         console.log('error: ' + err);
     });
 function appendData(data) {
-    var mainContainer = document.getElementById("middle");
+
+    const cards = [
+    document.getElementById("guide1"), 
+    document.getElementById("guide2"), 
+    document.getElementById("guide3")
+    ];
+    const cardTitle = [
+    document.getElementById("title1"), 
+    document.getElementById("title2"), 
+    document.getElementById("title3")    
+    ];
+    const cardBody = [
+        document.getElementById("body1"), 
+        document.getElementById("body2"), 
+        document.getElementById("body3")    
+    ];
+    const cardPDF = [
+        document.getElementById("pdf1"), 
+        document.getElementById("pdf2"), 
+        document.getElementById("pdf3")    
+    ];
+    const cardDOC = [
+        document.getElementById("doc1"), 
+        document.getElementById("doc2"), 
+        document.getElementById("doc3")    
+    ];
+
+    const highest = getHighest(data);
+
     for (var i = 0; i < data.length; i++) {
-        var div = document.createElement("div");
-        div.innerHTML = data[i].Name + ' ' + data[i].Date + ' <a href=../resources/' + data[i].pdf + '>PDF</a>' + ' <a href=' + data[i].doc + '>DOC</a>';
-        mainContainer.appendChild(div);
+        if (highest.includes(data[i].edition)){
+            cardTitle[0].innerHTML = "Raymer Handbook Edition " + data[i].edition;
+            cardBody[0].innerHTML = "Patch " + data[i].PatchNumber + " <br>Guide Released " + data[i].Date;
+            cardPDF[0].setAttribute('href', data[i].pdf);
+            cardDOC[0].setAttribute('href', data[i].doc);
+            cards[0].style.display = 'block';
+            cardTitle.shift();
+            cardBody.shift();
+            cardPDF.shift();
+            cardDOC.shift();
+            cards.shift();
+        }
     }
+    for (var i=0; i < cards.length; i++) {
+        cards[i].style.display = 'none';
+    }
+}
+function getHighest(data) {
+    const editionNumbers = [];
+    for (var i = 0; i < data.length; i++) {
+        editionNumbers[i] = data[i].edition;
+    }
+    editionNumbers.sort(function(a, b) { return b - a;});
+    return [editionNumbers[0],editionNumbers[1],editionNumbers[2]]
 }
